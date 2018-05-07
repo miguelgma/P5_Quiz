a813048e8e55033bd5079928ae601d7396b8e6ab
Miguel Garcia <miguel.gmaya@alumnos.upm.es>
2018-05-07 15:40:46 +0200

    quizzes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const quizController = require('../controllers/quiz');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,5 +9,17 @@ router.get('/', function(req, res, next) {
 router.get('/credits', function(req, res, next) {
   res.render('credits', { title: 'Creditos' });
 });
+
+router.get('/quizzes', quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
 
 module.exports = router;
